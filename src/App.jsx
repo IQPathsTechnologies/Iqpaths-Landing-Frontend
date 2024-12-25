675dba63fe79d5d30e1c05e1d1707e36bb3a9ae2
Hricha Sharma <sharmahricha6@gmail.com>
2024-12-25 11:39:15 +0530

    add course filters also hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import './App.css'
 import Header from './Components/Header/Header'
 import CourseSection from './Components/CourseSection/CourseSection'
 import CourseDetails from './Components/CourseSection/CourseDetails'
+import HeroSection from './Components/AllCourses/HeroSection'
+import SearchTab from './Components/AllCourses/SearchTab'
+import CourseFilter from './Components/AllCourses/CourseFilter'
 
 function App() {
 
   return (
     <>
       <Header />
-      <CourseSection />
-      <CourseDetails />
+      <HeroSection />
+      <SearchTab />
+      <CourseFilter />
+      {/* <CourseSection /> */}
+      {/* <CourseDetails /> */}
     </>
   )
 }
