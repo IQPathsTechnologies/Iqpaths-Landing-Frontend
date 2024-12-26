c66a740396320151d9cfd3deef45bc12b92c6525
Hricha Sharma <sharmahricha6@gmail.com>
2024-12-26 15:28:24 +0530

    add certificate and instructor

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import Header from './Components/Header/Header'
 import CourseSection from './Components/CourseSection/CourseSection'
 import CourseDetails from './Components/CourseSection/CourseDetails'
+import Certificate from './Components/Certificate/Certificate'
+import Instructor from './Components/Instructor/Instructor'
 import HeroSection from './Components/AllCourses/HeroSection'
 import SearchTab from './Components/AllCourses/SearchTab'
 import CourseFilter from './Components/AllCourses/CourseFilter'
@@ -12,12 +14,14 @@ function App() {
   return (
     <>
       <Header />
-      <HeroSection />
-      <SearchTab />
-      <CourseFilter />
-      <CourseCard />
-      {/* <CourseSection /> */}
-      {/* <CourseDetails /> */}
+      {/* <HeroSection /> */}
+      {/* <SearchTab /> */}
+      {/* <CourseFilter /> */}
+      {/* <CourseCard /> */}
+      <CourseSection />
+      <CourseDetails />
+      <Certificate />
+      <Instructor />
     </>
   )
 }
