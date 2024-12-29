3e882a887e61d7d2fe8f9083a0f217d6b5fa7390
Hricha Sharma <sharmahricha6@gmail.com>
2024-12-29 10:24:06 +0530

    add more description

--- src/Components/BenifitsSection/QueryCard.jsx
+++ src/Components/BenifitsSection/QueryCard.jsx
@@ -1,34 +1,23 @@
-import React, { useState } from 'react';
-import styles from './QueryCard.module.css';
+import React, { useState } from "react";
+import styles from "./QueryCard.module.css";
 
-function QueryCard({ title, description }) {
+function QueryCard({ title, shortDescription, longDescription }) {
   const [isExpanded, setIsExpanded] = useState(false);
-  const [showLimit, setShowLimit] = useState(100); // Limit for description characters
 
-  const toggleExpand = () => {
+  const toggleContent = () => {
     setIsExpanded(!isExpanded);
   };
 
-  const truncatedDescription = description.substring(0, showLimit);
-
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggleContent}>
       <div className={styles.dropdownIcon}>
         <img src="src/assets/Tick Square.png" alt="icon" />
       </div>
       <div className={styles.content}>
         <h4 className={styles.title}>{title}</h4>
         <p className={styles.description}>
-          {isExpanded ? description : `${truncatedDescription}...`}
-          <span className={styles.readMore} onClick={toggleExpand}>
-            {isExpanded ? ' Read Less' : ' Read More...'}
-          </span>
+          {isExpanded ? longDescription : shortDescription}
         </p>
-        {isExpanded && (
-          <div className={styles.expandedContent}>
-            <p>{description}</p>
-          </div>
-        )}
       </div>
     </div>
   );
