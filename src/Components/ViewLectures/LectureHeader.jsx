f2068baef4fb2f464694ee3891fcfe7cc5c7dfd8
Vishesh Verma <visheshverma250703@gmail.com>
2025-01-03 12:27:33 +0530

    Refactor LectureHeader component for improved readability; update CSS for better layout and remove unused styles

--- src/Components/ViewLectures/LectureHeader.jsx
+++ src/Components/ViewLectures/LectureHeader.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import {useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import styles from "./lectureHeader.module.css";
 
 const lectureHeader = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const pathSegments = location.pathname.split("/").filter((segment) => segment);
+  const pathSegments = location.pathname
+    .split("/")
+    .filter((segment) => segment);
 
   const handleBreadcrumbClick = (index) => {
     // Generate the path for the clicked breadcrumb
@@ -19,14 +21,24 @@ const lectureHeader = () => {
       <div className={styles.breadcrumb}>
         {/* Generate breadcrumbs dynamically */}
         <span>
-          <img src="/Home.png" alt="Home" onClick={() => navigate("/")} style={{ cursor: "pointer" }} />
+          <img
+            src="/Home.png"
+            alt="Home"
+            onClick={() => navigate("/")}
+            style={{ cursor: "pointer" }}
+          />
         </span>
         {pathSegments.map((segment, index) => (
           <React.Fragment key={index}>
-            <p> &gt; </p>
+            <div>
+              <img src="/CaretRight.svg" alt="Home" />
+            </div>
             <span
               onClick={() => handleBreadcrumbClick(index)}
-              style={{ cursor: index === pathSegments.length - 1 ? "default" : "pointer" }}
+              style={{
+                cursor:
+                  index === pathSegments.length - 1 ? "default" : "pointer",
+              }}
               className={index === pathSegments.length - 1 ? styles.active : ""}
             >
               {decodeURIComponent(segment)}
@@ -36,18 +48,17 @@ const lectureHeader = () => {
       </div>
       <div className={styles.actionIcons}>
         <div className={styles.buttonContainer}>
-          <div >
-            <img src="/prevLec.svg" alt="Home" /> 
+          <div>
+            <img src="/prevLec.svg" alt="Home" />
           </div>
           <p className={styles.previous}> Previous </p>
         </div>
         <div className={styles.buttonContainer}>
           <p className={styles.next}> Next </p>
           <div>
-            <img src="/nextLec.svg" alt="Home" /> 
+            <img src="/nextLec.svg" alt="Home" />
           </div>
         </div>
-
       </div>
     </header>
   );
