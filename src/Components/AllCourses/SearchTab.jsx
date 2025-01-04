09481cff67095751bdd95e280a2d7e4d7bbd589e
itish <itishjain7@gmail.com>
2025-01-04 23:31:34 +0530

    added search functionallity , created class for axios Search , changed courses and set courses from state to contexxt api , added Course Context and Wrapped in Main .jsx

--- src/Components/AllCourses/SearchTab.jsx
+++ src/Components/AllCourses/SearchTab.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
 import styles from './SearchTab.module.css';
 // import { FaSearch } from 'react-icons/fa'; 
+import {AllTypeOfSearch} from '../../axios/Search';
+import { CoursesContext } from '../../context/coursesContext';
 
 const SearchTab = () => {
+  const searchClass = new AllTypeOfSearch();
+  const{setCourses}=useContext(CoursesContext);
+  const dataWhichNeedToBeSearched = useRef(""); 
+  const handleWhatweWantToSearch =async()=>{
+    const response = await searchClass.search(dataWhichNeedToBeSearched.current.value);
+    console.log(response.data.data.courses);
+    // setCourses(response.data.data.courses);
+  }
   return (
     <div className={styles.overlay}>
     <div className={styles.searchBarWrapper}>
@@ -17,9 +27,10 @@ const SearchTab = () => {
                 type="text"
                 className={styles.searchInput}
                 placeholder="Lessons Name"
+                ref={dataWhichNeedToBeSearched}
             />
         </div>
-        <button className={styles.searchButton}>
+        <button className={styles.searchButton} onClick={handleWhatweWantToSearch}>
           <img src="/Search.png" alt="Search" />
           SEARCH
         </button>
