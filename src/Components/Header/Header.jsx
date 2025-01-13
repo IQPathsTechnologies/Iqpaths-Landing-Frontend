ce5488d9f594a5503c8dff1b3da90ce025d87963
Vishesh Verma <visheshverma250703@gmail.com>
2025-01-13 20:27:25 +0530

    feat: add responsive header and sidebar components with SVG icons

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,6 +8,7 @@ import { AuthService } from '../../axios/User';
 const Header = () => {
   const [isPopupVisible, setIsPopupVisible] = useState(false);
   const [popupType, setPopupType] = useState("");
+  const [isSideBarVisible, setIsSideBarVisible] = useState(false);
 
   const {  isLoggedIn, setIsLoggedIn } = useContext(UserContext);
 
@@ -59,62 +60,131 @@ const Header = () => {
     checkUser();
   }, []);
 
+
+  const openSideBar = () => {
+    setIsSideBarVisible(true);
+  };
+
+  const closeSideBar = () => {
+    setIsSideBarVisible(false);
+  }
+
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <img src="/logo.svg" alt="Paths Logo" className={styles.logoImage} />
-      </div>
-      <nav className={styles.navbar}>
-        <ul className={styles.navList}>
-          {navItems.map((tab) => (
-            <li key={tab.name} className={styles.navItem}>
-              <NavLink
-                to={tab.link}
-                className={({ isActive }) =>
-                  `${styles.link} ${isActive ? styles.active : ""}`
-                }
-              >
-                {tab.name}
-              </NavLink>
+    <>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <img src="/logo.svg" alt="Paths Logo" className={styles.logoImage} />
+        </div>
+        <nav className={styles.navbar}>
+          <ul className={styles.navList}>
+            {navItems.map((tab) => (
+              <li key={tab.name} className={styles.navItem}>
+                <NavLink
+                  to={tab.link}
+                  className={({ isActive }) =>
+                    `${styles.link} ${isActive ? styles.active : ""}`
+                  }
+                >
+                  {tab.name}
+                </NavLink>
+              </li>
+            ))}
+
+            <li
+              className={`${styles.hamburgerMenu}`}
+              onClick={openSideBar}
+            >
+              <img src="./hamburgerMenu.svg" alt="" />
             </li>
-          ))}
-        </ul>
-      </nav>
-      <div className={styles.lightBlue}>
-        <div className={styles.darkBlue}>
+          </ul>
+        </nav>
+        <div className={styles.lightBlue}>
+          <div className={styles.darkBlue}>
+            {isLoggedIn ? (
+              <div className={styles.authButtons}>
+                <NavLink to="/profile" className={styles.link}>
+                  <div className={styles.btn}>Profile</div>
+                </NavLink>
+                <div className={styles.btn} onClick={handleLogout}>
+                  Logout
+                </div>
+              </div>
+            ) : (
+              <div className={styles.authButtons}>
+                <NavLink
+                  to={{ pathname: "/signup", state: { type: "signup" } }}
+                  className={styles.link}
+                >
+                  <div className={styles.btn}>Sign Up</div>
+                </NavLink>
+                <NavLink
+                  to={{ pathname: "/login", state: { type: "login" } }}
+                  className={styles.link}
+                >
+                  <div className={styles.btn}>Login</div>
+                </NavLink>
+              </div>
+            )}
+          </div>
+        </div>
+
+        {/* Popup */}
+        {isPopupVisible && (
+          <SignUpPopup type={popupType} onClose={handleClosePopup} />
+        )}
+      </header>
+
+      {isSideBarVisible && (
+        <div className={styles.sidebar}>
+          <nav className={styles.navbar}>
+            <ul className={styles.navListSideBar}>
+              <li className={styles.navItemClose} onClick={closeSideBar}>
+                <img src="./HamburgerClose.svg" alt="" />
+              </li>
+              {navItems.map((tab) => (
+                <li key={tab.name} className={styles.navItemSideBar}>
+                  <NavLink
+                    to={tab.link}
+                    className={({ isActive }) =>
+                      `${styles.link} ${isActive ? styles.active : ""}`
+                    }
+                  >
+                    {tab.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
           {isLoggedIn ? (
-            <div className={styles.authButtons}>
+            <div>
               <NavLink to="/profile" className={styles.link}>
-                <div className={styles.btn}>Profile</div>
+                <div className={styles.btnSideBar}>Profile</div>
               </NavLink>
-              <div className={styles.btn} onClick={handleLogout}>
+              <div className={styles.btnSideBar} onClick={handleLogout}>
                 Logout
               </div>
             </div>
           ) : (
-            <div className={styles.authButtons}>
+            <div>
               <NavLink
-                to={{ pathname: '/signup', state: { type: 'signup' } }}
+                to={{ pathname: "/signup", state: { type: "signup" } }}
                 className={styles.link}
               >
-                <div className={styles.btn}>Sign Up</div>
+                <div className={styles.btnSideBar}>Sign Up</div>
               </NavLink>
               <NavLink
-                to={{ pathname: '/login', state: { type: 'login' } }}
+                to={{ pathname: "/login", state: { type: "login" } }}
                 className={styles.link}
               >
-                <div className={styles.btn}>Login</div>
+                <div className={`${styles.btnSideBar} ${styles.loginBtn}`}>
+                  Login
+                </div>
               </NavLink>
             </div>
           )}
         </div>
-      </div>
-
-      {/* Popup */}
-      {isPopupVisible && (
-        <SignUpPopup type={popupType} onClose={handleClosePopup} />
       )}
-    </header>
+    </>
   );
 };
 
