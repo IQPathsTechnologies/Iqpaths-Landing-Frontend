876654b294a9f5646dde89261ccffbb1d19a369b
Vishesh Verma <visheshverma250703@gmail.com>
2025-01-22 09:22:05 +0530

    fix(Banner): add skeleton loader for empty image array in carousel

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -31,15 +31,19 @@ const Banner = () => {
     , []);
 
     return (
-        // console.log('Banner :: images', images),
         <div className={styles.carousel}>
-            <ButtonCarousel autoslide={true}>
-                {images?.map((image, index) =>{
-                    // console.log(image);
-                    return <img src={image} alt="carousel" className={styles.carouselImage} key={index} />;
-                })}
-            </ButtonCarousel>
-            
+            {images.length > 0 ? (
+                <ButtonCarousel autoslide={true}>
+                    {images.map((image, index) => (
+                        <img src={image} alt="carousel" className={styles.carouselImage} key={index} />
+                    ))}
+                </ButtonCarousel>
+            ) : (
+                <div className={styles.skeleton}>
+                    {/* Add your skeleton loader here */}
+                    <p>Loading...</p>
+                </div>
+            )}
         </div>
     );
 };
