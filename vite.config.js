77dc5c155368b635d24db1916025d37d6a001fba
vivekdhawle <vivekdhawle02@gmail.com>
2025-01-22 13:21:42 +0530

    change in server config

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        // target: 'https://iqpath-landing-backend.onrender.com', 
-        // target: 'http://latestbackend.iqpaths.com', 
-        target: 'https://quiz.iqpaths.com', 
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      // '/api': "https://iqpath-landing-backend.onrender.com"
     },
   },
 });
