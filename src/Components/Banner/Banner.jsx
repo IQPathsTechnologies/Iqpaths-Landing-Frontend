c2386ba4e972ad8503caa6e10e113b77462e9117
Vishesh Verma <visheshverma250703@gmail.com>
2025-01-22 09:11:25 +0530

    bug fixed of home page map error

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -34,7 +34,7 @@ const Banner = () => {
         // console.log('Banner :: images', images),
         <div className={styles.carousel}>
             <ButtonCarousel autoslide={true}>
-                {images.map((image, index) =>{
+                {images?.map((image, index) =>{
                     // console.log(image);
                     return <img src={image} alt="carousel" className={styles.carouselImage} key={index} />;
                 })}
