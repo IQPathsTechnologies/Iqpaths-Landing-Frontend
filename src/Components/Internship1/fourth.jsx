cf933a99a795e3201b10dccafb5b5b9baeea9f65
Sourabh Lathi <lathisaurav@gmail.com>
2025-03-22 11:33:00 +0530

    fiter added to internship positions

--- src/Components/Internship1/fourth.jsx
+++ src/Components/Internship1/fourth.jsx
@@ -7,7 +7,35 @@ const apiClass = new AuthService();
 
 function Fourth() {
     const [positions, setPositions] = useState(null)
-    
+    // const position = [
+    //     { _id: "677d1bc4d5f03c47e237798d", title: "Full Stack Web Development", category: "Engineering" },
+    //     { _id: "678a80babf149ca1567ba608", title: "Web Development with React", category: "Engineering" },
+    //     { _id: "678a8100bf149ca1567ba609", title: "Backend Web Development", category: "Engineering" },
+    //     { _id: "677d1bc4d5f03c47e237768d", title: "Machine Learning", category: "Engineering" },
+    //     { _id: "677d1bc4d5f03c47e237793d", title: "Data Science", category: "Product" },
+    //     { _id: "67d968a577712cb5eab31b81", title: "Operations", category: "Operations" },
+    //     { _id: "67d96a5c77712cb5eab31b63", title: "Digital Marketing", category: "Marketing" },
+    //     { _id: "67d96b2f77712cb5eab36b64", title: "Video Editor", category: "Design" },
+    //     { _id: "67d96b8977712cb5eab31b65", title: "Graphic Designer", category: "Design" },
+    //   ];
+
+    const categories = [
+        { name: "All Positions", count: positions ? positions.length : 0 },
+        { name: "Engineering", count: positions ? positions.filter((c) => c.category === "Engineering").length : 0 },
+        { name: "Product", count: positions ? positions.filter((c) => c.category === "Product").length : 0 },
+        { name: "Design", count: positions ? positions.filter((c) => c.category === "Design").length : 0 },
+        { name: "Operations", count: positions ? positions.filter((c) => c.category === "Operations").length : 0 },
+        { name: "Marketing", count: positions ? positions.filter((c) => c.category === "Marketing").length : 0 },
+    ];
+
+    const [activeCategory, setActiveCategory] = useState("All Positions");
+
+    // Filter courses based on selected category
+    const filteredPositions =
+        activeCategory === "All Positions"
+            ? positions
+            : positions.filter((post) => post.category === activeCategory);
+
 
 
     useEffect(() => {
@@ -29,17 +57,21 @@ function Fourth() {
     return (
         <section id='fourth' className={styles.fourth}>
             <p className={styles.title}>
-                We have 17 open positions now!
+                We have {positions ? positions.length : 0} open positions now!
             </p>
             <div className={styles.wrapper}>
                 <div className={styles.left}>
                     <ul>
-                        <li className={styles.active}>All positions ({positions ? positions.length : 0})</li>
-                        <li>Enginering (7)</li>
-                        <li>Product (3)</li>
-                        <li>Design (1)</li>
-                        <li>Operation (4)</li>
-                        <li>Marketing (2)</li>
+                        {categories.map((cat) => (
+                            <li
+                                key={cat.name}
+                                className={`${activeCategory === cat.name ? styles.active : ""}`}
+
+                                onClick={() => setActiveCategory(cat.name)}
+                            >
+                                {cat.name} ({cat.count})
+                            </li>
+                        ))}
                     </ul>
                     <p className={styles.text}>We are always seeking talented people. In case you cannot find your desired position
                         here, please send us your LinkedIn profile and give us your contact information. We will be in
@@ -48,7 +80,7 @@ function Fourth() {
                 </div>
                 <div className={styles.right}>
                     <div>
-                        {positions ? positions.map((item, index) => (
+                        {positions ? filteredPositions.map((item, index) => (
                             <Card key={index} title={item.title} description={item.detailedDescription} />
                         )) : (
                             Array.from({ length: 4 }).map((_, index) => (
