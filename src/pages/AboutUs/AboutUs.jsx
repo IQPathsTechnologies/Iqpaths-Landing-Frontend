02cb59ca478b3c22264e4ea8ed1fb5fa6679871c
Sourabh Lathi <lathisaurav@gmail.com>
2025-03-23 15:55:52 +0530

    about us page scroll 0

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AboutHero from '../../Components/AboutUs/AboutHero';
 import EducationSection from '../../Components/AboutUs/EducationSection';
 import AboutBenefit from '../../Components/AboutUs/AboutBenefit';
 import NewsletterSection from '../../Components/AllCourses/NewsletterSection';
 
 const About = () => {
+     useEffect(() => {
+            window.scrollTo(0, 0);
+        }
+        , []);
     return (
         <div>
             <AboutHero />
