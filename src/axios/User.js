d15010bbdcbda205f39126f0602c87f6c66dfc0b
IQ-PathsTechnologies <iqpathsgit.resume@gmail.com>
2025-09-19 13:56:50 +0530

    change name  price

--- src/axios/User.js
+++ src/axios/User.js
@@ -238,7 +238,15 @@ export class AuthService {
         }
     }
 
-      
+    async getCourseDetails(courseId){
+        try {
+            const response =  await axios.post(`/api/courses/getCourseDetails`, {courseId}, {withCredentials: true});
+            return response.data.data;
+        } catch (error) {
+            // console.log("AuthService :: getCourseDetails :: error", error);
+            throw error;
+        }
+    }
     async getPurchasedCourseDetails(courseId){
         try {
             const response =  await axios.post(`/api/courses/getPurchasedCourseDetails`, {courseId}, {withCredentials: true});
