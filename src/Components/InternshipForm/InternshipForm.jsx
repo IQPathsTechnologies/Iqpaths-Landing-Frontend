710ef81ebead0ac745748e2f6d9d5cc94c0d22ed
Vishesh Verma <visheshverma250703@gmail.com>
2025-11-27 11:01:44 +0530

    refactor: update internship form fields and improve data handling

--- src/Components/InternshipForm/InternshipForm.jsx
+++ src/Components/InternshipForm/InternshipForm.jsx
@@ -1,12 +1,12 @@
-import { useState,  useEffect } from "react";
-import styles from "./InternshipForm.module.css";
-import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { internshipFormSchema } from "./validationSchemas";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { LuLoaderCircle } from "react-icons/lu";
+import { useParams } from "react-router-dom";
 import { AuthService } from "../../axios/User";
 import { notifyError, notifySuccess } from "../../utility/Tostify/Tosts";
-import { useParams } from "react-router-dom";
-import { LuLoaderCircle } from "react-icons/lu";
+import styles from "./InternshipForm.module.css";
+import { internshipFormSchema } from "./validationSchemas";
 
 const Form = () => {
   const { title } = useParams();
@@ -19,7 +19,7 @@ const Form = () => {
     handleSubmit,
     formState: { errors, isValid },
     reset,
-    watch
+    watch,
   } = useForm({
     resolver: zodResolver(internshipFormSchema),
     mode: "onChange",
@@ -30,7 +30,7 @@ const Form = () => {
   const onSubmit = (data) => {
     setSubmitting(true);
     const file = data.resume[0];
-    data.position = title;
+    data.internshipSelection = title;
 
     const allowedTypes = [
       "application/pdf",
@@ -57,13 +57,15 @@ const Form = () => {
 
   const handleFormSubmit = async (data, file) => {
     const formData = new FormData();
-    formData.append("name", data.name);
+    formData.append("fullName", data.fullName);
     formData.append("email", data.email);
-    formData.append("mobileNo", data.mobileNo);
-    formData.append("college", data.college);
-    formData.append("year", data.year);
-    formData.append("github", data.github || "");
-    formData.append("position", title);
+    formData.append("phoneNumber", data.phoneNumber);
+    formData.append("collegeUniversity", data.collegeUniversity);
+    formData.append("programCourse", data.programCourse);
+    formData.append("yearOfStudy", data.yearOfStudy);
+    formData.append("linkedinUrl", data.linkedinUrl || "");
+    formData.append("portfolioUrl", data.portfolioUrl || "");
+    formData.append("internshipSelection", title);
     formData.append("resume", file);
 
     try {
@@ -80,7 +82,6 @@ const Form = () => {
     }
   };
 
-
   useEffect(() => {
     const subscription = watch(() => {
       setBookingResponse(null);
@@ -88,99 +89,166 @@ const Form = () => {
     return () => subscription.unsubscribe();
   }, [watch]);
 
-
   return (
     <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <h2 className={styles.heading}>{title} Application form</h2>
       <div className={styles.main}>
         <div>
           <div className={styles.label}>
-            <label htmlFor="">Name <span className={styles.required}>*</span></label>
+            <label htmlFor="">
+              Name <span className={styles.required}>*</span>
+            </label>
             <input
               type="text"
               placeholder="full name"
-              {...register("name")}
+              {...register("fullName")}
               className={styles.input}
             />
-            {errors.name && <span className={styles.error}>{errors.name.message}</span>}
+            {errors.fullName && (
+              <span className={styles.error}>{errors.fullName.message}</span>
+            )}
           </div>
 
           <div className={styles.label}>
-            <label htmlFor="">E-mail <span className={styles.required}>*</span></label>
+            <label htmlFor="">
+              E-mail <span className={styles.required}>*</span>
+            </label>
             <input
               type="email"
               placeholder="email"
               {...register("email")}
               className={styles.input}
             />
-            {errors.email && <span className={styles.error}>{errors.email.message}</span>}
+            {errors.email && (
+              <span className={styles.error}>{errors.email.message}</span>
+            )}
           </div>
 
           <div className={styles.label}>
-            <label htmlFor="">Contact No <span className={styles.required}>*</span></label>
+            <label htmlFor="">
+              Contact No <span className={styles.required}>*</span>
+            </label>
             <input
               type="tel"
               placeholder="contact"
-              {...register("mobileNo")}
+              {...register("phoneNumber")}
               className={styles.input}
             />
-            {errors.mobileNo && <span className={styles.error}>{errors.mobileNo.message}</span>}
+            {errors.phoneNumber && (
+              <span className={styles.error}>{errors.phoneNumber.message}</span>
+            )}
           </div>
 
           <div className={styles.label}>
-            <label htmlFor="">College Name <span className={styles.required}>*</span></label>
+            <label htmlFor="">
+              College/University <span className={styles.required}>*</span>
+            </label>
             <input
               type="text"
-              placeholder="college name"
-              {...register("college")}
+              placeholder="college/university name"
+              {...register("collegeUniversity")}
               className={styles.input}
             />
-            {errors.college && <span className={styles.error}>{errors.college.message}</span>}
+            {errors.collegeUniversity && (
+              <span className={styles.error}>
+                {errors.collegeUniversity.message}
+              </span>
+            )}
+          </div>
+
+          <div className={styles.label}>
+            <label htmlFor="">
+              Program/Course <span className={styles.required}>*</span>
+            </label>
+            <input
+              type="text"
+              placeholder="e.g., B.Tech CSE, BCA, MCA"
+              {...register("programCourse")}
+              className={styles.input}
+            />
+            {errors.programCourse && (
+              <span className={styles.error}>
+                {errors.programCourse.message}
+              </span>
+            )}
           </div>
         </div>
 
         <div>
           <div className={styles.lab}>
-            <label htmlFor="">Year/Experienced <span className={styles.required}>*</span></label>
-            <select {...register("year")} className={styles.select} defaultValue="">
-              <option value="" disabled>Select</option>
+            <label htmlFor="">
+              Year of Study <span className={styles.required}>*</span>
+            </label>
+            <select
+              {...register("yearOfStudy")}
+              className={styles.select}
+              defaultValue=""
+            >
+              <option value="" disabled>
+                Select
+              </option>
               <option value="first year">1st Year</option>
               <option value="second year">2nd Year</option>
               <option value="third year">3rd Year</option>
               <option value="fourth year">4th Year</option>
               <option value="experienced">Experienced</option>
             </select>
-            {errors.year && <span className={styles.error}>{errors.year.message}</span>}
+            {errors.yearOfStudy && (
+              <span className={styles.error}>{errors.yearOfStudy.message}</span>
+            )}
           </div>
 
           <div className={styles.label}>
-            <label htmlFor="">GitHub <span className={styles.required}>*</span></label>
+            <label htmlFor="">LinkedIn URL</label>
             <input
               type="url"
-              placeholder="github (optional)"
-              {...register("github")}
+              placeholder="linkedin profile (optional)"
+              {...register("linkedinUrl")}
               className={styles.input}
             />
-            {errors.github && <span className={styles.error}>{errors.github.message}</span>}
+            {errors.linkedinUrl && (
+              <span className={styles.error}>{errors.linkedinUrl.message}</span>
+            )}
+          </div>
+
+          <div className={styles.label}>
+            <label htmlFor="">Portfolio/GitHub</label>
+            <input
+              type="url"
+              placeholder="portfolio or github (optional)"
+              {...register("portfolioUrl")}
+              className={styles.input}
+            />
+            {errors.portfolioUrl && (
+              <span className={styles.error}>
+                {errors.portfolioUrl.message}
+              </span>
+            )}
           </div>
 
           <div className={styles.resume}>
-            <label htmlFor="">Upload Resume <span className={styles.required}>*</span></label>
+            <label htmlFor="">
+              Upload Resume <span className={styles.required}>*</span>
+            </label>
             <input
               type="file"
               {...register("resume")}
               className={styles.input}
             />
-            {errors.resume && <span className={styles.error}>{errors.resume.message}</span>}
+            {errors.resume && (
+              <span className={styles.error}>{errors.resume.message}</span>
+            )}
             {uploadError && <span className={styles.error}>{uploadError}</span>}
           </div>
 
-          {bookingResponse && <span className={styles.success}>{bookingResponse}</span>}
+          {bookingResponse && (
+            <span className={styles.success}>{bookingResponse}</span>
+          )}
 
-           <button
+          <button
             className={styles.btn}
             disabled={!isValid || submitting}
-            style={{ backgroundColor: !isValid ? "#4D7FD1" : undefined, }}  // light background
+            style={{ backgroundColor: !isValid ? "#4D7FD1" : undefined }} // light background
           >
             {submitting ? (
               <LuLoaderCircle size={30} className={styles.loader} />
@@ -188,8 +256,6 @@ const Form = () => {
               "Submit"
             )}
           </button>
-
-
         </div>
       </div>
     </form>
